2e1cf3e5b3274b892d83a7bb0bc942385ea4a84e
RodrigoNeoEx <draja.acc@gmail.com>
2021-06-08 09:59:56 -0300

    Updates

---
+++ precache-manifest.3f4bca1fbbc1d7d8e46fea69b699224a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d85659d9865666b8d6092406a84c7f5",
+    "url": "/wallet/index.html"
+  },
+  {
+    "revision": "44d62afc409a309d82f9",
+    "url": "/wallet/static/css/main.7984a8a5.chunk.css"
+  },
+  {
+    "revision": "6cca5a229091d0c8212a",
+    "url": "/wallet/static/js/2.d597e0eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wallet/static/js/2.d597e0eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d62afc409a309d82f9",
+    "url": "/wallet/static/js/main.e8cc18e8.chunk.js"
+  },
+  {
+    "revision": "32abb29ede4798555f19",
+    "url": "/wallet/static/js/runtime-main.86db26d2.js"
+  },
+  {
+    "revision": "21700388cb1d56515dbd635a89976a1f",
+    "url": "/wallet/static/media/echangeImg.21700388.jpg"
+  }
+]);
